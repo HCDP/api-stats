a11857363939f176b8a33576a4d8975fbe363144
princessbobsanta <princessbobsantaclaus@gmail.com>
2023-02-24 11:10:26 -1000

    basic files and testing

---
+++ index.js
@@ -0,0 +1,30 @@
+function refresh() {
+    console.log("refresh");
+    getData();
+}
+
+
+function getData() {
+    let div = document.getElementById("data");
+    let url = "https://cistore.its.hawaii.edu:8443/apistats"
+
+    fetch(url, {})
+   .then((response) => {
+       console.log(response);
+       return response.json();
+   })
+   .then((json) =>  {
+       console.log(json);
+       div.innerHTML = json;
+   });
+}
+
+function bytes2gb(bytes) {
+    let gb = bytes / 1024 / 1024 / 1024;
+    gb = Math.round(gb * 100) / 100;
+    return `${gb}GB`;
+}
+
+window.addEventListener("load", (event) => {
+    getData();
+});
